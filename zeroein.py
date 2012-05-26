e5ed969f7c05974df1153ba7d1ddc383d0094ad7
Takafumi Arakaki <aka.tkf@gmail.com>
2012-05-26 02:44:46 +0200

    Prepare modules when starting zeroein.py

--- zeroein.py
+++ zeroein.py
@@ -4,12 +4,127 @@
 Zero setup Emacs IPython Notebook client
 """
 
+import os
+ZEROEIN_ROOT = os.path.dirname(__file__)
+
+
+class BaseTask(object):
+
+    cwd = ZEROEIN_ROOT
+    parents = []
+    pconfig = {}
+    done = False
+
+    def run(self):
+        self.start()
+        self.wait()
+
+    def start(self):
+        if self.done:
+            return
+        tasks = []
+        for p in self.parents:
+            tasks.append(p(**self.pconfig.get(p, {})))
+        for t in tasks:
+            t.start()
+        for t in tasks:
+            t.wait()
+
+    def wait(self):
+        pass
+
+
+class BaseBlockingTask(BaseTask):
+
+    def start(self):
+        for p in self.parents:
+            t = p(**self.pconfig.get(p, {}))
+            t.start()
+            t.wait()
+
+
+class BaseCommandTask(BaseTask):
+
+    command = []
+    proc = None
+
+    def start(self):
+        import subprocess
+        super(BaseCommandTask, self).start()
+        self.proc = subprocess.Popen(
+            self.command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=self.cwd or None)
+
+    def wait(self):
+        if self.proc:
+            if self.proc.wait() != 0:
+                print self.proc.stdout.read()
+                print self.proc.stderr.read()
+                raise RuntimeError("An error occurred while running {0}"
+                                   .format(self.command))
+
+
+class BaseGitModuleTask(BaseCommandTask):
+
+    subcommand = None
+
+    def __init__(self, reponame):
+        self.reponame = reponame
+
+    @property
+    def command(self):
+        return ['git', 'submodule', self.subcommand, self.reponame]
+
+
+class GitModuleInit(BaseGitModuleTask):
+    subcommand = 'init'
+
+
+class GitModuleUpdate(BaseGitModuleTask):
+    subcommand = 'update'
+
+
+class BaseModuleTask(BaseBlockingTask):
+
+    reponame = None
+    parents = [GitModuleInit, GitModuleUpdate]
+
+    def __init__(self):
+        super(BaseModuleTask, self).__init__()
+        config = {'reponame': self.reponame}
+        self.pconfig = {GitModuleInit: config, GitModuleUpdate: config}
+        if os.path.isdir(os.path.join(self.cwd, self.reponame, '.git')):
+            self.done = True
+
+
+def make_module_task(_reponame):
+    class Task(BaseModuleTask):
+        reponame = _reponame
+    return Task
+
+
+class PrepareModulesTask(BaseTask):
+
+    parents = map(
+        make_module_task,
+        ["fuzzy", "pos-tip", "smartrep", "auto-complete", "python",
+         "websocket", "markdown-mode", "nxhtml", "popup", "ein"])
+
+
+class ZeroEINTask(BaseCommandTask):
+
+    parents = [PrepareModulesTask]
+
+    def __init__(self, command):
+        self.command = command
+
 
 def zeroein(emacs):
-    import subprocess
-    import os
-    elisp = os.path.join(os.path.dirname(__file__), 'zeroein.el')
-    subprocess.Popen([emacs, '-Q', '-l', elisp])
+    elisp = os.path.join(ZEROEIN_ROOT, 'zeroein.el')
+    task = ZeroEINTask([emacs, '-Q', '-l', elisp])
+    task.start()
 
 
 def main():
